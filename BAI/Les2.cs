1b93163cab51d5012061fb7f64fb3b86022ac44d
Mark Heijnekamp <mark@heijnekamp.nl>
2022-03-02 22:41:58 +0100

    Add project files.

---
+++ BAI/Les2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAI
+{
+    internal class Les2
+    {
+        int[,] StedenKM = new int[,]
+        {
+            {0,50,135,120,65,65 },
+            {50,0,140,75,20,70 },
+            {135,140,0,220,170,75 },
+            {120,75,220,0,55,145 },
+            {65,20,170,55,0,90 },
+            {65,70,75,145,90,0 }
+        };
+        String[] Steden = new string[]
+            {
+        "Arnhem",
+        "Amersfoort",
+        "Assen",
+        "Rotterdam",
+        "Utrecht",
+        "Zwolle"
+            };
+
+    }
+}
---
+++ BAI/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int[,] StedenKM = new int[,]
+{
+    {0,50,135,120,65,65 },
+    {50,0,140,75,20,70 },
+    {135,140,0,220,170,75 },
+    {120,75,220,0,55,145 },
+    {65,20,170,55,0,90 },
+    {65,70,75,145,90,0 }
+};
+String Steden = new string[]
+{
+    "Arnhem",
+    "Amersfoort",
+    "Assen",
+    "Rotterdam",
+    "Utrecht",
+    "Zwolle"
+}
