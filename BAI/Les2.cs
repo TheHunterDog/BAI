06b973072d5b1d6b6526b532e1986d5683c07bd5
Mark Heijnekamp <mark@heijnekamp.nl>
2022-03-03 16:43:49 +0100

    Les2 loot

--- BAI/Les2.cs
+++ BAI/Les2.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 
 namespace BAI
 {
-    internal class Les2
+    static class Les2
     {
-        int[,] StedenKM = new int[,]
+        public static int[,] StedenKM = new int[,]
         {
             {0,50,135,120,65,65 },
             {50,0,140,75,20,70 },
@@ -17,7 +17,7 @@ namespace BAI
             {65,20,170,55,0,90 },
             {65,70,75,145,90,0 }
         };
-        String[] Steden = new string[]
+        public static String[] Steden = new string[]
             {
         "Arnhem",
         "Amersfoort",
@@ -26,6 +26,24 @@ namespace BAI
         "Utrecht",
         "Zwolle"
             };
-
+        public static void calcMinDist()
+        {
+            int res = 0;
+            int Total = 0;
+            for (int i = 0; i < StedenKM.GetLength(0); i++)
+            {
+                int Calc = 0;
+                for (int j = 0; j < StedenKM.GetLength(1); j++)
+                {
+                    Calc += StedenKM[i, j];
+                }
+                if(Total < Calc)
+                {
+                    res = i;
+                    Total = Calc;
+                }
+            }
+            Console.WriteLine($"Het beste is als in: {Steden[res]} een les wordt gegeven omdat dit over het algemeen de minste afstand is voor de meeste studenten.");
+        }
     }
 }
